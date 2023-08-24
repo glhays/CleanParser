cb3997363c6170cce3dad2bad6b9e08ab8f4fc50
Greg Hays <glhays.dev@gmail.com>
2023-08-23 19:42:58 -0700

    INFRA: Initialize Unit Tests Project
    Closes: #3

---
+++ CleanParser.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,18 @@
+// ---------------------------------------------------------------
+// Copyright (c) Gregory Hays All rights reserved.
+// Licensed under the BSD 3-Clause License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Xunit;
+
+namespace CleanParser.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+            true.Should().BeTrue();
+    }
+}
