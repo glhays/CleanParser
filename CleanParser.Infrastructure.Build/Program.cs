78edbb88e6070f4bb942fbbb52b4daf841a8b4fb
Greg Hays <glhays.dev@gmail.com>
2023-08-25 18:42:01 -0700

    INFRA: Initialize Infrastructure
    Closes: #5

---
+++ CleanParser.Infrastructure.Build/Program.cs
@@ -0,0 +1,84 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Gregory Hays All rights reserved.
+// Licensed under the BSD 3-Clause License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace CleanParser.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ADotNetClient();
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = "CleanParser Build",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "main" }
+                    },
+
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "main" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.WindowsLatest,
+
+                        Steps = new List<GithubTask>
+                        {
+                            new CheckoutTaskV3
+                            {
+                                Name = "Checking Out Code"
+                            },
+
+                            new SetupDotNetTaskV1
+                            {
+                                Name = "Install .Net",
+
+                                TargetDotNetVersion = new TargetDotNetVersion
+                                {
+                                    DotNetVersion = "7.0.10"
+                                }
+                            },
+
+                            new RestoreTask
+                            {
+                                Name = "Restoring Nuget Packages"
+                            },
+
+                            new DotNetBuildTask
+                            {
+                                Name = "Building Project"
+                            },
+
+                            new TestTask
+                            {
+                                Name = "Running Tests"
+                            }
+                        },
+                    }
+                }
+            };
+
+            client.SerializeAndWriteToFile(adoPipeline: githubPipeline,
+                path: "../../../../.github/workflows/dotnet.yml");
+        }
+    }
+}
