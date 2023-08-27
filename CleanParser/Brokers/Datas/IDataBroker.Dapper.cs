196189b3d2c32bc26dbcd0a3ad17a0fa96bd292d
Greg Hays <glhays.dev@gmail.com>
2023-08-27 14:14:15 -0700

    BROKERS: Datas DataBroker Dapper
    Closes: #13

---
+++ CleanParser/Brokers/Datas/IDataBroker.Dapper.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ---------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanParser.Brokers.Datas
+{
+    internal partial interface IDataBroker
+    {
+        ValueTask<IEnumerable<T>> RetrieveDataAsync<T>(string query);
+    }
+}
