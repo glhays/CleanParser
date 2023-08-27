b7550be5dbc6ce6c18d08d2a25e6a54126fb548e
Greg Hays <glhays.dev@gmail.com>
2023-08-27 14:00:54 -0700

    DATA: Datas Models DataConfigurations
    Closes: #12

---
+++ CleanParser/Models/Datas/DataConfigurations.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ---------------------------------------------------------------------------------------------
+
+namespace CleanParser.Models.Datas
+{
+    internal  class DataConfigurations
+    {
+        public string ConnectionString { get; set; }
+    }
+}
