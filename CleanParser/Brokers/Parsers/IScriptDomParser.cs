0b52436a4d5015ddc4a5dbd7069763ceede73cf5
Greg Hays <glhays.dev@gmail.com>
2023-08-28 20:19:24 -0700

    BROKERS: Parsers ScriptDom
    Closes: #17

---
+++ CleanParser/Brokers/Parsers/IScriptDomParser.cs
@@ -0,0 +1,16 @@
+﻿// -------------------------------------------------------------------------------------------
+// Copyright (c) The Standard Community, a coalition of the Good-Hearted Engineers
+// -------------------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace CleanParser.Brokers.Parsers
+{
+    public interface IScriptDomParser
+    {
+        string ParseQuery(string queryText, out IList<ParseError> parseErrors);
+    }
+}
