7403e386a83d75c2e28391107e0583609e682c40
Greg Hays <glhays.dev@gmail.com>
2024-08-26 16:02:43 -0700

    FIRST PR

---
+++ CleanParser/Services/Foundations/Class.cs
@@ -0,0 +1,16 @@
+﻿// -------------------------------------------------------------------------------------------
+// Copyright (c) The Standard Community, a coalition of the Good-Hearted Engineers
+// -------------------------------------------------------------------------------------------
+
+namespace CleanParser.Services.Foundations
+{
+    internal class Class
+    {
+        public string Name { get; set; }
+        public string Namespace { get; set; }
+        public string FullName => $"{Namespace}.{Name}";
+        public string FileName => $"{Name}.cs";
+        public string FilePath => $"{Namespace}\\{FileName}";
+        public string Content { get; set; }
+    }
+}
